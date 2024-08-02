3ace8eb6f734efe760d570e88221033e34a27b31
Mattia Sicuro <msicuro33@gmail.com>
2024-08-02 17:41:07 -0400

    Remove space from node name

--- AimNode/AimNode/pluginMain.cpp
+++ AimNode/AimNode/pluginMain.cpp
@@ -5,7 +5,7 @@ MStatus initializePlugin(MObject plugin)
 {
 	MStatus stat;
 	MFnPlugin fnPlugin(plugin, "Mattia Sicuro", "1.0", "Any", &stat); CHECK_MSTATUS_AND_RETURN_IT(stat);
-	CHECK_MSTATUS_AND_RETURN_IT(fnPlugin.registerNode("Aim Node", AimNode::typeID, AimNode::creator, AimNode::initialize));
+	CHECK_MSTATUS_AND_RETURN_IT(fnPlugin.registerNode("AimNode", AimNode::typeID, AimNode::creator, AimNode::initialize));
 	return MS::kSuccess;
 }
 
